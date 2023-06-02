e06e5f5b6b62f661c58bddae016e405131de9a01
Thalles Passos <contato@thalles.me>
2023-06-02 09:42:48 -0300

    fix: percentages are not parsed correctly in zod

--- src/managers/validators/CoinsManager.validator.ts
+++ src/managers/validators/CoinsManager.validator.ts
@@ -50,6 +50,10 @@ export const coinMarketSchema = z.object({
       price: z.array(z.number()),
     })
     .nullish(),
+  price_change_percentage_14d_in_currency: z.number().nullish(),
+  price_change_percentage_1h_in_currency: z.number().nullish(),
+  price_change_percentage_1y_in_currency: z.number().nullish(),
+  price_change_percentage_200d_in_currency: z.number().nullish(),
   price_change_percentage_24h_in_currency: z.number().nullish(),
   price_change_percentage_30d_in_currency: z.number().nullish(),
   price_change_percentage_7d_in_currency: z.number().nullish(),
