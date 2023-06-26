5d8e9e01d164a0c9e2026902905b5d515e03f8d0
Thalles Passos <contato@thalles.me>
2023-06-26 15:59:20 -0300

    refactor: no scientific notation

--- src/utils/schemaparser.ts
+++ src/utils/schemaparser.ts
@@ -1,16 +1,15 @@
 import { parse } from "lossless-json";
 import { Response } from "node-fetch";
 import { InvalidResponseFromCoingeckoError } from "../errors/InvalidResponseFromCoingeckoError.js";
+import { BigNumber } from "bignumber.js";
 
 export async function parseSchemaFromResponse<T>(
   schema: Zod.ZodTypeAny,
   response: Response
 ): Promise<T> {
   try {
-    const jsonData = parse(
-      await response.text(),
-      undefined,
-      (value: string) => value
+    const jsonData = parse(await response.text(), undefined, (value: string) =>
+      new BigNumber(value).toString()
     );
 
     const data = await schema.parseAsync(jsonData).catch((err) => {
